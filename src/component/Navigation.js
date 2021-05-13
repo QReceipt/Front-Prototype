da91cb3c7279934e7700bda724164e9723bb663a
C12H22O12 <director20844@gmail.com>
2021-05-13 23:10:02 +0900

    21/05/13 23:09 이수민과 컴포넌트 병합 후 라우터 추가

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,13 +1,16 @@
 import React from "react";
 import './Navigation.css';
+import {Link} from"react-router-dom"
 
 function Navigation() {
     return (<div>
         <nav>
             <ul>
-                <li role="presentation" className="active">영수증 상세보기</li>
+                
+                <li role="presentation"><Link to ="/">메인</Link></li>
+                <li role="presentation" className="active"><Link to ="/Detail">영수증 상세보기</Link></li>
                 <li role="presentation">영수증 모아보기</li>
-                <li role="presentation">회원가입</li>
+                <li role="presentation"><Link to ="/signup">회원가입</Link></li>
             </ul>
         </nav>
     </div>
