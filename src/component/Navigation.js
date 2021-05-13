d9afb88048a40d499673acc35ff5fda670d1fe81
C12H22O12 <director20844@gmail.com>
2021-05-14 02:33:11 +0900

    21/05/14 2:32 라우터 적용

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Navigation.css';
 import styled from 'styled-components';
+import {Link} from "react-router-dom"
 
 let NaviBody = styled.div`
     padding: 0;
@@ -21,9 +22,10 @@ let Ul = styled.li`
 function Navigation() {
     return (<NaviBody>
             <Ul>
-                <li class="plusFun">영수증 상세보기</li>
-                <li class="plusFun">영수증 모아보기</li>
-                <li class="plusFun">회원가입</li>
+                <li class="plusFun" role="presentation"><Link to="/">메인</Link></li>
+                <li class="plusFun active" role="presentation"><Link to="/Detail">영수증 상세보기</Link></li>
+                <li class="plusFun" role="presentation">영수증 모아보기</li>
+                <li class="plusFun" role="presentation"><Link to="/signup">회원가입</Link></li>
             </Ul>
      </NaviBody>
 
