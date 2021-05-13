e6c0c8d712b18491671f516f9d72e10d73a497da
leesumin <leesumin-223@github.com>
2021-05-13 21:16:40 +0900

    21/3/13 21:16 병합준비

---
+++ src/component/Temp.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Detail from "./Detail"
+import '../App.css';
+
+
+function Temp() {
+  return (
+    <div className="Temp">
+      <Detail
+      address="헤라클레스"
+      phoneNum="010111111"
+      shopOrder="무기징역"
+      deliveryOrder="로건 리 사망"
+      spoonCheck="X"
+      origin="오윤희 자백"
+      />
+    </div>
+  );
+}
+
+export default Temp;
