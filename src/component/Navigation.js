33015a2e876e1f8ff710d6cb89a03366002fc25e
C12H22O12 <director20844@gmail.com>
2021-05-16 14:48:24 +0900

    21/05/16 14:48 사이드바 형태 만듦

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -19,9 +19,16 @@ let Ul = styled.ul `
     top:0;
 `;
 
+let H3 = styled.h3`
+    margin-top:0;
+    margin-bottom:20px;
+    padding:0;
+`;
+
 function Navigation() {
     return (
         <NaviBody>
+            <Sidebar/>
             <Ul>
                 <li className="navLi" role="presentation">
                     <Link className="plusFun" to="/">메인</Link>
@@ -34,14 +41,35 @@ function Navigation() {
                     <Link className="plusFun" to="/signup">회원가입</Link>
                 </li>
             </Ul>
-            <div id="menubar">
+            <button id="menubar">
                 <div className="hambuger"></div>
                 <div className="hambuger"></div>
                 <div className="hambuger"></div>
-            </div>
+            </button>
         </NaviBody>
 
     )
+}
+
+function Sidebar() {
+    return (
+        <div className="talkbubble">
+            <H3>MENU</H3>
+            <ul>
+                <li role="presentation">
+                    <Link className="plusFunS" to="/">메인</Link>
+                </li>
+                <li role="presentation">
+                    <Link className="plusFunS active" to="/Detail">영수증 상세보기</Link>
+                </li>
+                <li className="navLi plusFunS" role="presentation">영수증 모아보기</li>
+                <li role="presentation">
+                    <Link className="plusFunS" to="/signup">회원가입</Link>
+                </li>
+            </ul>
+        </div>
+    );
+
 }
 
 export default Navigation;
